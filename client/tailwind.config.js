9a5d5ebed5d7bd6ea19604c74a87750f3d930023
abeidahmed <abeidahmed92@gmail.com>
2020-08-30 11:39:55 +0530

    install custom font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
